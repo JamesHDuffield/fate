ce4d50097e017d4e2f3bd61aa477a6c258cd8a37
James Duffield <james.h.duffield@gmail.com>
2019-03-14 16:21:36 +1100

    Add firebase and angular material init

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { environment } from '../environments/environment';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'fate';
+
+  ngOnInit() {
+    firebase.initializeApp(environment.firebase);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA_dEwTK7U3S9Wj-2-I3lv_UN4J06zaCV4',
+    authDomain: 'fate-read.firebaseapp.com',
+    databaseURL: 'https://fate-read.firebaseio.com',
+    projectId: 'fate-read',
+    storageBucket: 'fate-read.appspot.com',
+    messagingSenderId: '844222987550'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
