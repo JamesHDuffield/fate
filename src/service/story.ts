a7da39cbeb98fe4cba2dfc61bd92e9036fb8aa16
James Duffield <james.h.duffield@gmail.com>
2019-03-16 17:03:22 +1100

    Add reference to story service

--- src/service/story.ts
+++ src/service/story.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { Moment } from 'src/models/moment';
 
 @Injectable()
 export class StoryService {
 
-    cursor: Moment;
+    cursor: DocumentReference;
 
     constructor(private db: AngularFirestore) {}
 
     async fetchMoment(id: string): Promise<Moment> {
-        const item = await this.db.collection('moment').doc(id).get().toPromise();
-        this.cursor = item.data() as Moment;
-        return this.cursor;
+        this.cursor = this.db.collection('moment').doc(id).ref;
+        return (await this.cursor.get()).data() as Moment;
     }
 
-    async createMoment(optionText: string, previousMoment: Moment): Promise<Moment> {
+    async createMoment(optionText: string): Promise<Moment> {
         const moment = {
             text: 'And then...'
         };
         const ref = await this.db.collection('moment').add(moment);
-        const newMoment = await ref.get();
 
         const option = {
             text: optionText,
-            id: newMoment.id,
+            id: ref.id,
         };
-        previousMoment.options.push(option);
-        // this.db.collection('moment').doc()
-        return newMoment.data() as Moment;
+        await this.cursor.update({
+            options: [ option ]
+        });
+        this.cursor = ref;
+        return (await this.cursor.get()).data() as Moment;
     }
 
 }
