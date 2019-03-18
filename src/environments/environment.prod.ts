472f68c77a133643154ce98fe94d03facf94d154
James Duffield <james.duffield@fusion5.com.au>
2019-03-18 14:47:26 +1100

    Deploy this to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA_dEwTK7U3S9Wj-2-I3lv_UN4J06zaCV4',
+    authDomain: 'fate-read.firebaseapp.com',
+    databaseURL: 'https://fate-read.firebaseio.com',
+    projectId: 'fate-read',
+    storageBucket: 'fate-read.appspot.com',
+    messagingSenderId: '844222987550'
+  }
 };
