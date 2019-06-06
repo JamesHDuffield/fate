6f7a365495af56090f6e9793c2a793041d6d9d8b
James Duffield <james.duffield@fusion5.com.au>
2019-06-06 15:15:26 +1000

    Add fade in animation

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -3,6 +3,9 @@ import { StoryService } from '../../service/story';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Option } from '../../models/moment';
+import { delay, tap, throttleTime } from 'rxjs/operators';
+
+const ANIMATION_DELAY = 300;
 
 @Component({
   selector: 'app-story',
@@ -13,9 +16,9 @@ import { Option } from '../../models/moment';
       state('in', style({ opacity: 1 })),
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('600ms 300ms'),
+        animate(`600ms ${ANIMATION_DELAY}ms`),
       ]),
-      transition(':leave', animate(300, style({ opacity: 0 }))),
+      transition(':leave', animate(ANIMATION_DELAY, style({ opacity: 0 }))),
     ]),
   ],
 })
@@ -23,11 +26,18 @@ export class StoryComponent {
   customOption = false;
   customText = false;
   text = new FormControl('');
+  hideForAnimation = false;
   optionForm = new FormGroup({
     text: new FormControl(''),
     type: new FormControl('moment'),
   });
-  current$ = this.story.current$;
+  current$ = this.story.current$
+    .pipe(
+      throttleTime(ANIMATION_DELAY),
+      tap(() => this.hideForAnimation = true),
+      delay(ANIMATION_DELAY),
+      tap(() => this.hideForAnimation = false),
+    );
 
   constructor(private story: StoryService) { }
 
