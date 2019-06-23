9e4c24843729c54d9dc8aa2d2430a96faa7338ea
James Duffield <james.h.duffield@gmail.com>
2019-06-23 17:21:45 +1000

    More logging on create user

--- functions/src/operations/userCreate.ts
+++ functions/src/operations/userCreate.ts
@@ -2,7 +2,8 @@ import { DatabaseService } from '../services/db';
 import * as admin from 'firebase-admin';
 
 export const userCreate = async (user: admin.auth.UserRecord, db: DatabaseService) => {
-  console.log(user);
+  console.log('User', user);
   await db.createUser(user.uid, user.displayName);
+  console.log('Created user');
   return;
 };
